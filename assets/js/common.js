2271be108757eb5d7cdc270f37921a898fc7aeb8
Ken <s1110434045@gms.nutc.edu.tw>
2021-12-13 21:59:00 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,3 +2,12 @@ $(function() {
     $("#header").load("./header.html");
     $("#footer").load("./footer.html");
 });
+
+function showDropdown(obj) {
+    $('.header-dropdown').slideUp(300);
+    $(obj).closest('div').find('.header-dropdown').slideDown(300);
+}
+
+function hideDropdown() {
+    $('.header-dropdown').slideUp(300);
+}
